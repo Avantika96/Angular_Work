c26e2fff0a6ddb41b9d307af7d00242c9961383a
Avantika96 <avantikagupta.96@gmail.com>
2017-06-29 14:20:36 +0530

    Login/Signup

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NavbarComponent} from './navbar/navbar.component';
+import {BodyComponent} from './body/body.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  img="C:\Avantika\internship17\practice_angular\myapp\src\image\back1.jpg";
+}
